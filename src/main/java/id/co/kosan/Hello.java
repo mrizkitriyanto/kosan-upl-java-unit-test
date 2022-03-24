bc321cdc4bbc906e74ccda6eff05ed70952a273c
mrizkitriyanto <muhamadrizkitriyanto41@gmail.com>
2022-03-24 11:46:58 +0700

    add dependency

---
+++ src/main/java/id/co/kosan/Hello.java
@@ -0,0 +1,7 @@
+package id.co.kosan;
+
+public class Hello {
+	public String SayHello( ) {
+		return "Hello";
+	}
+}
---
+++ src/test/java/id/co/kosan/HelloTest.java
@@ -0,0 +1,16 @@
+package id.co.kosan;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloTest {
+
+	@Test
+	void test() {
+		Hello hello = new Hello();
+		String aktual = hello.SayHello();
+		assertEquals("Hello", aktual);
+	}
+
+}
